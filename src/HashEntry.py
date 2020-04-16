f2b853e9f84555b46f05da0ac312e65e157bd0f5
Coen M <coenmeulenkamp@gmail.com>
2020-04-16 17:21:17 +0200

    Overengineerd the HashEntry

--- src/HashEntry.py
+++ src/HashEntry.py
@@ -1,14 +1,28 @@
 class HashEntry:
     """
-    A class to store an Hashed Entity
+    A class to store a Hashed Entity
 
     """
     def __init__(self, value):
-        """
-        To 
-
-        :value The value that needs to be stored
-        """
         self.value = value
-        self.key = hash(value)
+
+    @property
+    def value(self):
+        return self.__value
+    
+    @value.setter
+    def value(self, newValue):
+        self.__value = newValue
+        self.key = hash(newValue)
+
+    @property
+    def key(self):
+        return hash(self.value)
+    
+    @key.setter
+    def key(self, newKey):
+        pass
+
+    def __hash__(self):
+        return hash(self.value)
 
